87be2b74c76dce9234d6b3973536eb32f5b79466
lazybones1 <lazybones1@naver.com>
2020-05-21 01:10:13 +0900

    운체 2차 과제 완료

--- 운영체제/2차 과제/Dining Philosophers Problem/개선 파일.c
+++ 운영체제/2차 과제/Dining Philosophers Problem/개선 파일.c
@@ -17,29 +17,27 @@ sem_t mutex; //상호 배제
 sem_t S[N]; //세마포어
 
 void show_state(){
-   for(int i = 0; i<N; i++){
-    printf(" |phil %d : %d|",i+1, state[i]);
-  }
-  printf("\n");
+ for(int i = 0; i<N; i++){
+ printf(" |phil %d : %d|",i+1, state[i]);
+ }
+ printf("\n");
 }
 
 void show_graph(int i){
-  sem_wait(&mutex);
-  printf(" -----------------------------------------\n");
-  printf(" phil : %d end\n", i+1);
-  show_state();
-  for(int i = 0; i<N; i++){
-    int r = (i+1)%5;
-
-    if(state[i] == 0){
-      printf(" ───> |phil %d| <─── ",i+1);
-
-    }else if(state[i] == 1){
-      printf(" ---> |phil %d| <--- ",i+1);
-    }else if(state[i] == 2){
-      printf(" |phil %d| ",i+1);
-    }
-    printf("|fork %d|", i+1);
+ sem_wait(&mutex);
+ printf(" -----------------------------------------\n");
+ printf(" phil : %d end\n", i+1);
+ show_state();
+ for(int i = 0; i<N; i++){
+  int r = (i+1)%5;
+  if(state[i] == 0){
+   printf(" ───> |phil %d| <─── ",i+1);
+  }else if(state[i] == 1){
+   printf(" ---> |phil %d| <--- ",i+1);
+  }else if(state[i] == 2){
+   printf(" |phil %d| ",i+1);
+  }
+   printf("|fork %d|", i+1);
   }
   printf("\n");
   printf(" -----------------------------------------\n");
@@ -48,80 +46,76 @@ void show_graph(int i){
 
 void test(int phnum)
 {
-	if (state[phnum] == HUNGRY
-		&& state[LEFT] != EATING
-		&& state[RIGHT] != EATING) {
-		// state that eating
-		state[phnum] = EATING;
-		sleep(2);
-		printf("Philosopher %d takes fork %d and %d\n",
-					phnum + 1, LEFT + 1, phnum + 1);
-		printf("Philosopher %d is Eating\n", phnum + 1);
-		// sem_post(&S[phnum]) has no effect
-		// during takefork
-		// used to wake up hungry philosophers
-		// during putfork
-		sem_post(&S[phnum]); //세마포어 증가
-	}
+ if (state[phnum] == HUNGRY && state[LEFT] != EATING
+  && state[RIGHT] != EATING) {
+  // state that eating
+ state[phnum] = EATING;
+ sleep(2);
+ printf("Philosopher %d takes fork %d and %d\n",
+  phnum + 1, LEFT + 1, phnum + 1);
+ printf("Philosopher %d is Eating\n", phnum + 1);
+ // sem_post(&S[phnum]) has no effect
+ // during takefork
+ // used to wake up hungry philosophers
+ // during putfork
+ sem_post(&S[phnum]); //세마포어 증가
+ }
 }
 
 // take up chopsticks
 void take_fork(int phnum)
 {
-	sem_wait(&mutex); //세마포어 감소
-	// state that hungry
-	state[phnum] = HUNGRY; //배고픔 상태로 변경
-	printf("Philosopher %d is Hungry\n", phnum + 1);
-	// eat if neighbours are not eating
-	test(phnum); //양옆에서 먹고 있는 경우 세마포어 증가
-	sem_post(&mutex); //세마포어 증가
-	// if unable to eat wait to be signalled
-	sem_wait(&S[phnum]); //세마포어 감소
-	sleep(1);
+ sem_wait(&mutex); //세마포어 감소
+ // state that hungry
+ state[phnum] = HUNGRY; //배고픔 상태로 변경
+ printf("Philosopher %d is Hungry\n", phnum + 1);
+ // eat if neighbours are not eating
+ test(phnum); //양옆에서 먹고 있는 경우 세마포어 증가
+ sem_post(&mutex); //세마포어 증가
+ // if unable to eat wait to be signalled
+ sem_wait(&S[phnum]); //세마포어 감소
+ sleep(1);
 }
 
 // put down chopsticks
 void put_fork(int phnum)
 {
-	sem_wait(&mutex); //세마포어 감소
-	// state that thinking
-	state[phnum] = THINKING; //생각상태로 변경
-	printf("Philosopher %d putting fork %d and %d down\n",
-		phnum + 1, LEFT + 1, phnum + 1);
-	printf("Philosopher %d is thinking\n", phnum + 1);
+ sem_wait(&mutex); //세마포어 감소
+ // state that thinking
+ state[phnum] = THINKING; //생각상태로 변경
+ printf("Philosopher %d putting fork %d and %d down\n",
+  phnum + 1, LEFT + 1, phnum + 1);
+ printf("Philosopher %d is thinking\n", phnum + 1);
 
-	test(LEFT);
-	test(RIGHT);
-	sem_post(&mutex); //세마포어 증가
+ test(LEFT);
+ test(RIGHT);
+ sem_post(&mutex); //세마포어 증가
 }
 
 void* philospher(void* num)
 {
-	while (1) {
-		int* i = num;
-		sleep(1);
-		take_fork(*i); //포크 잡기
-		sleep(0);
-		put_fork(*i); //포크 두기
-    show_graph(*i);
-	}
+ while (1) {
+  int* i = num;
+  sleep(1);
+  take_fork(*i); //포크 잡기
+  sleep(0);
+  put_fork(*i); //포크 두기
+  show_graph(*i);
+ }
 }
 
 int main()
 {
-	int i;
-	pthread_t thread_id[N]; //스레드형 배열
+ int i;
+ pthread_t thread_id[N]; //스레드형 배열
+ sem_init(&mutex, 0, 1); // initialize the semaphores
+ for (i = 0; i < N; i++) // initialize the semaphores
+  sem_init(&S[i], 0, 0);
+ for (i = 0; i < N; i++) { 	// create philosopher processes
+  pthread_create(&thread_id[i], NULL,philospher, &phil[i]);
+  printf("Philosopher %d is thinking\n", i + 1);
+ }
 
-	sem_init(&mutex, 0, 1); // initialize the semaphores
-	for (i = 0; i < N; i++) // initialize the semaphores
-		sem_init(&S[i], 0, 0);
-
-	for (i = 0; i < N; i++) { 	// create philosopher processes
-			pthread_create(&thread_id[i], NULL,
-					philospher, &phil[i]);
-		printf("Philosopher %d is thinking\n", i + 1);
-	}
-
-	for (i = 0; i < N; i++) // 쓰레드 종료 대기
-		pthread_join(thread_id[i], NULL);
+ for (i = 0; i < N; i++) // 쓰레드 종료 대기
+  pthread_join(thread_id[i], NULL);
 }
--- 운영체제/2차 과제/Producer-Consumer Problem/개선 파일.c
+++ 운영체제/2차 과제/Producer-Consumer Problem/개선 파일.c
@@ -8,87 +8,86 @@ int* buf; //버퍼, 처음은 0으로 모두 초기화
 
 int main()
 {
-	int n;
-  buf = (int*)malloc(sizeof(int)*buf_size);
-	void producer();
-	void consumer();
-  void show_status();
-	int wait(int);
-	int signal(int);
-	printf("\n1.Producer\n2.Consumer\n3.Show_Status\n4.Exit");
-	while(1)
-	{
-		printf("\nEnter your choice:");
-		scanf("%d",&n);
-		switch(n)
-		{
-			case 1:	if((mutex==1)&&(x!=buf_size))
-						producer();
-					else
-						printf("Buffer is full!!");
-					break;
-			case 2:	if((mutex==1)&&(x!=0))
-						consumer();
-					else
-						printf("Buffer is empty!!");
-					break;
-      case 3: if(mutex==1)
-          show_status();
-          break;
-			case 4:
-					exit(0);
-					break;
-		}
-	}
-
-	return 0;
+ int n;
+ buf = (int*)malloc(sizeof(int)*buf_size);
+ void producer();
+ void consumer();
+ void show_status();
+ int wait(int);
+ int signal(int);
+ printf("\n1.Producer\n2.Consumer\n3.Show_Status\n4.Exit");
+ while(1)
+ {
+  printf("\nEnter your choice:");
+  scanf("%d",&n);
+  switch(n)
+  {
+	  case 1: if((mutex==1)&&(x!=buf_size))
+				  producer();
+			  else
+				  printf("Buffer is full!!");
+			  break;
+	  case 2:	if((mutex==1)&&(x!=0))
+				  consumer();
+			  else
+				  printf("Buffer is empty!!");
+			  break;
+    case 3: if(mutex==1)
+        show_status();
+        break;
+	  case 4:
+			  exit(0);
+			  break;
+	 }
+ }
+ return 0;
 }
 
 int wait(int s)
 {
-	return (--s);
+ return (--s);
 }
 
 int signal(int s)
 {
-	return(++s);
+ return(++s);
 }
 
 void show_status(){
-  int i;
-  mutex = wait(mutex);
-  printf("-------Buffer status------\n");
-  for(i=0; i<buf_size; i++){
-    printf("Buffer %d : %d | ", i+1, buf[i]); // 버퍼의 상태 (1 = 아이템 있음, 0 = 아이템 없음)
-  }
-  printf("\n");
-  printf("Buffer size : %d\n",buf_size); //총 버퍼 크기
-  printf("Producer position : %d\n", p_pos); //생산자 위치
-  printf("Customer position : %d\n", c_pos); //소비자 위치
-  printf("Number of items : %d\n", x); //아이템 수
-  printf("Empty slot : %d\n", buf_size-x); //빈 슬롯 수
-  printf("---------------------------\n");
-  mutex=signal(mutex);
+ int i;
+ mutex = wait(mutex);
+ printf("-------Buffer status------\n");
+ for(i=0; i<buf_size; i++){
+   printf("Buffer %d : %d | ", i+1, buf[i]); // 버퍼의 상태 (1 = 아이템 있음, 0 = 아이템 없음)
+ }
+ printf("\n");
+ printf("Buffer size : %d\n",buf_size); //총 버퍼 크기
+ printf("Producer position : %d\n", p_pos); //생산자 위치
+ printf("Customer position : %d\n", c_pos); //소비자 위치
+ printf("Number of items : %d\n", x); //아이템 수
+ printf("Empty slot : %d\n", buf_size-x); //빈 슬롯 수
+ printf("---------------------------\n");
+ mutex=signal(mutex);
 }
 
 void producer()
 {
-	mutex=wait(mutex); //상호 배제 on
-  if(p_pos!=buf_size) p_pos++; //생산자의 위치 조정
-  else p_pos = 1;
-  buf[p_pos-1] = 1; // 버퍼에 값 추가
-	x++; //아이템 수 증가
-	printf("\nProducer produces the item %d\n",x);
-	mutex=signal(mutex); //상호 배제 off
+ mutex=wait(mutex); //상호 배제 on
+ if(p_pos!=buf_size) p_pos++; //생산자의 위치 조정
+ else p_pos = 1;
+ buf[p_pos-1] = 1; // 버퍼에 값 추가
+ x++; //아이템 수 증가
+ printf("\nProducer produces the item %d\n",x);
+ mutex=signal(mutex); //상호 배제 off
 }
 
 void consumer()
 {
-	mutex=wait(mutex); //상호 배제 on
-  buf[c_pos] = 0; //버퍼에 값 제거
-  if(c_pos!=buf_size) c_pos++; //소비자의 위치 조정
-  else c_pos = 1;
-	printf("\nConsumer consumes item %d\n",x);
-	x--; //아이템 수 감소
-	mutex=signal(mutex); //상호 배제 off
+ mutex=wait(mutex); //상호 배제 on
+ buf[c_pos] = 0; //버퍼에 값 제거
+ if(c_pos!=buf_size) c_pos++; //소비자의 위치 조정
+ else c_pos = 1;
+ printf("\nConsumer consumes item %d\n",x);
+ x--; //아이템 수 감소
+ mutex=signal(mutex); //상호 배제 off
 }
