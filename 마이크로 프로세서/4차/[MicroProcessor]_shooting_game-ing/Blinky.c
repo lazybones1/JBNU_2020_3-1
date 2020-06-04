71f3041e926bff41ba19d810a7c4926dd3e86e45
lazybones1 <lazybones1@naver.com>
2020-06-04 15:49:42 +0900

    점수 기능 추가

--- 마이크로 프로세서/4차/[MicroProcessor]_shooting_game-ing/Blinky.c
+++ 마이크로 프로세서/4차/[MicroProcessor]_shooting_game-ing/Blinky.c
@@ -17,21 +17,7 @@
 #include "GLCD.h"
 #include "LED.h"
 
-
-OS_TID t_phaseA;                        /* assigned task id of task: phase_a */
-OS_TID t_phaseB;                        /* assigned task id of task: phase_b */
-OS_TID t_phaseC;                        /* assigned task id of task: phase_c */
-OS_TID t_phaseD;                        /* assigned task id of task: phase_d */
-OS_TID t_clock;                         /* assigned task id of task: clock   */
-OS_TID t_lcd;                           /* assigned task id of task: lcd     */
-
 OS_MUT mut_GLCD;                        /* Mutex to controll GLCD access     */
-
-#define LED_A      LED_0
-#define LED_B      LED_1
-#define LED_C      LED_2
-#define LED_D      LED_3
-#define LED_CLK    LED_7
 
 #define KBD_SELECT      0x01               
 #define KBD_LEFT        0x08               
@@ -63,40 +49,38 @@ int missile_posX[100];
 int missile_posY[100];
 int missile_num;
 
+int point;
 
 char fighter_shape;
 char meteo_shape;
 char missile_shape;
 
-void createMeteo(){
-	//srand( (unsigned)time(NULL) );  
+void createMeteo(){	
 	meteo_posX = 300;
-	meteo_posY = (rand()%200) + 24;	
+	meteo_posY = (rand()%Line7) + 24;	
 	GLCD_displayChar(meteo_posX, meteo_posY, meteo_shape);	
 }
-//void meteoCrash(){
-//	int i = 0;
-//	while(i <50){		
-//		if(meteo_posY <= missile_posY[i] && meteo_posY+24 >= missile_posY[i] && meteo_posX <=missile_posX[i] && isMissile[i]==1){
-//			isMissile[i] = 0;
-//			GLCD_displayChar(meteo_posX, meteo_posY, ' ');
-//			GLCD_displayChar(missile_posX[i], missile_posY[i], ' ');			
-//			createMeteo();
-//			break;
-//		}	
-//		i++;
-//	}	
-//}
+
 void missileMove(){
 	int i =0;
 	while(i<missile_max){		
 		if(isMissile[i]==1){
 			if(meteo_posY-12 <= missile_posY[i] && meteo_posY+12 >= missile_posY[i] && meteo_posX <=missile_posX[i]){
+				int tmpp;
+				int x = 100, y = Line9;
 				isMissile[i] = 0;
 				GLCD_displayChar(meteo_posX, meteo_posY, ' ');
 				GLCD_displayChar(missile_posX[i], missile_posY[i], ' ');			
 				createMeteo();
 				meteo_speed*=1.1;
+				point++;
+				tmpp = point;
+				while(tmpp>0){
+					char tmp = (char)(tmpp%10+'0');	
+					GLCD_displayChar(x, y, tmp);
+					x-=20;
+					tmpp = tmpp/10;
+				}
 				break;
 			}	
 			GLCD_displayChar(missile_posX[i], missile_posY[i], ' ');
@@ -121,15 +105,28 @@ void fire(){
 void gameOver(){
 	GLCD_clear(Black);
 	GLCD_setBackColor(Black);
-	GLCD_setTextColor(White);
-	if(isPassed == 3){
+	GLCD_setTextColor(White);	
+	if(isPassed == 0){
 		GLCD_displayStringLn(Line1, "     GAME OVER     ");
-		GLCD_displayStringLn(Line3, "  You Missed Meteo ");
+		GLCD_displayStringLn(Line2, "  You Missed Meteo ");
 	}
 	else{
 		GLCD_displayStringLn(Line1, "     GAME OVER  ");
-		GLCD_displayStringLn(Line3, "   fighter Crashed");
+		GLCD_displayStringLn(Line2, "   fighter Crashed");
 	}
+	GLCD_displayStringLn(Line3, "  Your Point : ");	
+	if(point == 0){
+		GLCD_displayChar(260, Line3, '0');
+	}
+	else{
+		int x=260, y=Line3;
+		while(point>0){
+			char tmp = (char)(point%10+'0');	
+			GLCD_displayChar(x, y, tmp);
+			x-=20;
+			point = point/10;
+		}	
+	}	
 	GLCD_displayStringLn(Line5, "  Push RESET Button ");	
 	while(1){
 	}	
@@ -139,8 +136,9 @@ void moveMeteo(){
 	meteo_posX -= meteo_speed;	
 	GLCD_displayChar(meteo_posX, meteo_posY, meteo_shape);
 	if(meteo_posX <= 1) {
-		isPassed += 1;
-		if(isPassed == 3){
+		isPassed -= 1;
+		GLCD_displayChar(200, Line9, (char)(isPassed+'0'));
+		if(isPassed == 0){
 			gameOver();
 		}
 		GLCD_displayChar(meteo_posX, meteo_posY, ' ');
@@ -148,7 +146,7 @@ void moveMeteo(){
 	}
 }
 void fighterCrash(){
-	if((meteo_posY == fighter_posY) && (meteo_posX <= fighter_posX +20)){
+	if((meteo_posY+10 >= fighter_posY && meteo_posY -10<= fighter_posY) &&(meteo_posX <= fighter_posX +20)){
 		gameOver();
 	}
 }
@@ -161,7 +159,7 @@ void fighterMove(int key){
 		else fighter_posY -= 4;
 	}
 	else if(key == down) {
-		if(fighter_posY == 240-24) fighter_posY = 240-24;
+		if(fighter_posY == Line8) fighter_posY = Line8;
 		else fighter_posY += 4;
 	}
 	else if(key == right){
@@ -171,6 +169,7 @@ void fighterMove(int key){
 	GLCD_displayChar(tmpX, tmpY, ' ');
 	GLCD_displayChar(fighter_posX, fighter_posY, fighter_shape);
 }
+
 void gameInit(){
 	fighter_posX = 0;
 	fighter_posY = 120;	
@@ -180,15 +179,17 @@ void gameInit(){
 	meteo_speed = 5;
 	key = -1;
 	isShoot = 0;
-	isPassed = 0;	
+	isPassed = 3;	
 	inGame = 0;
-	
+	point = 0;
 	fighter_shape = '>';
 	meteo_shape ='O';
 	missile_shape ='-';
 	
 	GLCD_setBackColor(White);
 	GLCD_setTextColor(Black);
+	GLCD_displayChar(100, Line9, (char)(point+'0'));
+	GLCD_displayChar(200, Line9, (char)(isPassed+'0'));
 }
 
 void joyStick (void) {
@@ -208,7 +209,6 @@ void joyStick (void) {
 	}
 }
 
-
 void runGame(){
 	gameInit();
 	inGame = 1;
@@ -217,144 +217,16 @@ void runGame(){
 		joyStick();	
 		fighterMove(key);
 		missileMove();
-		moveMeteo();
-		//meteoCrash();
+		moveMeteo();		
 		fighterCrash();
 	}
 }
 
-/*----------------------------------------------------------------------------
- *        Function 'signal_func' called from multiple tasks
- *---------------------------------------------------------------------------*/
-//void signal_func (OS_TID task)
-//{
-//	os_evt_set (0x0100, t_clock);          /* send event signal to clock task  */
-//	os_dly_wait (50);                      /* delay 50 clock ticks             */
-//	os_evt_set (0x0100, t_clock);          /* send event signal to clock task  */
-//	os_dly_wait (50);                      /* delay 50 clock ticks             */
-//	os_evt_set (0x0001, task);             /* send event to task 'task'        */
-//	os_dly_wait (50);                      /* delay 50 clock ticks             */
-//}
-
-/*----------------------------------------------------------------------------
- *        Task 1 'phaseA': Phase A output
- *---------------------------------------------------------------------------*/
-//__task void phaseA (void)
-//{
-//	for (;;)
-//	{
-//		os_evt_wait_and (0x0001, 0xffff);    /* wait for an event flag 0x0001    */
-//		LED_On (LED_A);
-//		signal_func (t_phaseB);              /* call common signal function      */
-//		LED_Off(LED_A);
-//	}
-//}
-
-/*----------------------------------------------------------------------------
- *        Task 2 'phaseB': Phase B output
- *---------------------------------------------------------------------------*/
-//__task void phaseB (void)
-//{
-//	for (;;)
-//	{
-//		os_evt_wait_and (0x0001, 0xffff);    /* wait for an event flag 0x0001    */
-//		LED_On (LED_B);
-//		signal_func (t_phaseC);              /* call common signal function      */
-//		LED_Off(LED_B);
-//	}
-//}
-
-/*----------------------------------------------------------------------------
- *        Task 3 'phaseC': Phase C output
- *---------------------------------------------------------------------------*/
-//__task void phaseC (void)
-//{
-//	for (;;)
-//	{
-//		os_evt_wait_and (0x0001, 0xffff);    /* wait for an event flag 0x0001    */
-//		LED_On (LED_C);
-//		signal_func (t_phaseD);              /* call common signal function      */
-//		LED_Off(LED_C);
-//	}
-//}
-
-/*----------------------------------------------------------------------------
- *        Task 4 'phaseD': Phase D output
- *---------------------------------------------------------------------------*/
-//__task void phaseD (void)
-//{
-//	for (;;)
-//	{
-//		os_evt_wait_and (0x0001, 0xffff);    /* wait for an event flag 0x0001    */
-//		LED_On (LED_D);
-//		signal_func (t_phaseA);              /* call common signal function      */
-//		LED_Off(LED_D);
-//	}
-//}
-
-/*----------------------------------------------------------------------------
- *        Task 5 'clock': Signal Clock
- *---------------------------------------------------------------------------*/
-//__task void clocks (void)
-//{
-//	for (;;)
-//	{
-//		os_evt_wait_and (0x0100, 0xffff);    /* wait for an event flag 0x0100    */
-//		LED_On (LED_CLK);
-//		os_dly_wait (8);                     /* delay 8 clock ticks              */
-//		LED_Off(LED_CLK);
-//	}
-//}
-
-/*----------------------------------------------------------------------------
- *        Task 6 'lcd': LCD Control task
- *---------------------------------------------------------------------------*/
-//__task void lcd (void)
-//{
-//	for (;;)
-//	{
-//		GLCD_setTextColor(Black);                          /* Set the Text Color */			
-//		GLCD_displayChar(0, 60, 'o');
-//		GLCD_displayChar(0, 40, 'o');
-//		GLCD_displayChar(0, 20, 'o');	
-//	}
-//}
-
-/*----------------------------------------------------------------------------
- *        Task 7 'init': Initialize
- *---------------------------------------------------------------------------*/
-
-//__task void init (void)
-//{
-//	os_mut_init(mut_GLCD);
-//
-//	t_phaseA = os_tsk_create (phaseA, 0);  /* start task phaseA                */
-//	t_phaseB = os_tsk_create (phaseB, 0);  /* start task phaseB                */
-//	t_phaseC = os_tsk_create (phaseC, 0);  /* start task phaseC                */
-//	t_phaseD = os_tsk_create (phaseD, 0);  /* start task phaseD                */
-//	t_clock  = os_tsk_create (clocks, 0);   /* start task clock                 */
-//	t_lcd    = os_tsk_create (lcd, 0);     /* start task lcd                   */
-//	os_evt_set (0x0001, t_phaseA);         /* send signal event to task phaseA */
-//	os_tsk_delete_self ();
-//}
-
-/*----------------------------------------------------------------------------
- *        Main: Initialize and start RTX Kernel
- *---------------------------------------------------------------------------*/
 int main (void)
 {
-//	SystemInit();                             /* Initialize the MCU clocks     */
-
-//	LED_init ();                              /* Initialize the LEDs           */
-
 	GLCD_init();                              /* Initialize the GLCD           */
 	GLCD_clear(White);                        /* Clear the GLCD                */
 	
 	runGame();
-	//GLCD_displayChar(0, 24, 'o');
-	//os_sys_init(init);                        /* Initialize RTX and start init */
-}
 
-/*----------------------------------------------------------------------------
- * end of file
- *---------------------------------------------------------------------------*/
+}
